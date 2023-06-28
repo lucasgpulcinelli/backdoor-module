52ca43e10857fdaef269c19232e9f0936cc81564
lucasgpulcinelli <lucasgpulcinelli@users.noreply.github.com>
2023-06-28 08:24:27 -0300

    solving issue with client, now fully works

--- client/client.py
+++ client/client.py
@@ -1,33 +1,30 @@
 import socket
 import struct
 import os
+import time
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((os.environ['COMPUTER_IP'], 8080))
 
-res = s.recv(4096)
-if not res:
-    quit(-1)
-print(res.decode())
+res = bytes()
+while len(res) != 4096:
+    res += s.recv(4096-len(res))
+print(res.decode()[::-1])
 
-res = s.recv(8)
-if not res:
-    quit(-1)
+res = None
+while not res:
+    res = s.recv(8)
 x, y = struct.unpack("ii", res)
 
-l = 0
-final_buf = bytes()
-while l < 3*x*y:
-    res = s.recv(3*x*y-l)
-    if not res:
-        break
-    l += len(res)
-    final_buf += res
+res = bytes()
+while len(res) < 3*x*y:
+    res += s.recv(3*x*y-len(res))
+    time.sleep(0.1)
 
 f = open("out.ppm", "wb")
 f.write(b"P6\n")
 f.write(f"{x} {y}\n".encode())
 f.write(b"255\n")
-f.write(final_buf)
+f.write(res)
 
 s.close()
