66e0738dc860f5557086bae927af70bdd57b00de
lucasgpulcinelli <lucasgpulcinelli@users.noreply.github.com>
2023-06-27 23:49:29 -0300

    ppm output file correct

--- client/client.py
+++ client/client.py
@@ -1,34 +1,35 @@
 import socket
 import struct
 import os
-import time
 
-while True:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((os.environ['COMPUTER_IP'], 8080))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((os.environ['COMPUTER_IP'], 8080))
 
-    res = s.recv(4096)
+res = s.recv(4096)
+if not res:
+    quit(-1)
+print(res.decode())
+
+res = s.recv(8)
+if not res:
+    quit(-1)
+x, y = struct.unpack("ii", res)
+print(f'{x} {y}')
+
+l = 0
+final_buf = bytes()
+while l < 3*x*y:
+    res = s.recv(3*x*y-l)
     if not res:
         break
-    print(res.decode())
+    l += len(res)
+    final_buf += res
+    print(f'got {l} bytes')
 
-    res = s.recv(8)
-    if not res:
-        break
-    x, y = struct.unpack("ii", res)
-    print(f'{x} {y}')
+f = open("out.ppm", "wb")
+f.write(b"P6\n")
+f.write(b"1440 900\n")
+f.write(b"255\n")
+f.write(final_buf)
 
-    l = 0
-    while l < 3*x*y:
-        res = s.recv(3*x*y-l)
-        if not res:
-            break
-        l += len(res)
-        print(f'got {l} bytes')
-
-    s.close()
-    time.sleep(10)
-
-print("error, socket did not send data")
 s.close()
-
